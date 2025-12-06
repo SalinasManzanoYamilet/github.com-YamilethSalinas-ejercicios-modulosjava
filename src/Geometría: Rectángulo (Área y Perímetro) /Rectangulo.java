bcbc646eddeb720b1c901a9cd0c8263719e2bc33
SalinasManzanoYamilet <salinasmanzanoyamilet@gmail.com>
2025-12-05 20:36:27 -0600

    Create Rectangulo.java

---
+++ src/Geometría: Rectángulo (Área y Perímetro) /Rectangulo.java
@@ -0,0 +1,44 @@
+public class Rectangulo {
+
+    private double base;
+    private double altura;
+    private String etiqueta;
+
+    public Rectangulo(double base, double altura, String etiqueta) {
+        this.base = base;
+        this.altura = altura;
+        this.etiqueta = etiqueta;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public String getEtiqueta() {
+        return etiqueta;
+    }
+
+    public void setEtiqueta(String etiqueta) {
+        this.etiqueta = etiqueta;
+    }
+
+    public double calcularArea() {
+        return base * altura;
+    }
+
+    public double calcularPerimetro() {
+        return (2 * base) + (2 * altura);
+    }
+}
