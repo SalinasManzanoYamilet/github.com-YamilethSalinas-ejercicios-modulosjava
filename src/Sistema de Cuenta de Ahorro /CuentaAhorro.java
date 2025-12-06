49cc1387822487bfb575a8b37c8930571a05f83a
SalinasManzanoYamilet <salinasmanzanoyamilet@gmail.com>
2025-12-05 20:40:11 -0600

    Create CuentaAhorro.java

---
+++ src/Sistema de Cuenta de Ahorro /CuentaAhorro.java
@@ -0,0 +1,50 @@
+public class CuentaAhorro {
+
+    private String numeroCuenta;
+    private String titular;
+    private double saldo;
+
+    public CuentaAhorro(String numeroCuenta, String titular, double saldo) {
+        this.numeroCuenta = numeroCuenta;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void depositar(double cantidad) {
+        if (cantidad > 0) {
+            saldo += cantidad;
+        }
+    }
+
+    public boolean retirar(double cantidad) {
+        if (cantidad > 0 && cantidad <= saldo) {
+            saldo -= cantidad;
+            return true;
+        }
+        return false;
+    }
+}
