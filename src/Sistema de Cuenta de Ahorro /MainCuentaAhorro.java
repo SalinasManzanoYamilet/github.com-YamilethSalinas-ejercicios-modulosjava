48d59ee9c715c8ee82412aa2e5d0c5fabb919140
SalinasManzanoYamilet <salinasmanzanoyamilet@gmail.com>
2025-12-05 20:41:14 -0600

    Create MainCuentaAhorro.java

---
+++ src/Sistema de Cuenta de Ahorro /MainCuentaAhorro.java
@@ -0,0 +1,8 @@
+public class MainCuentaAhorro {
+
+    public static void main(String[] args) {
+        ControlCuentaAhorro control = new ControlCuentaAhorro();
+        control.ejecutar();
+    }
+
+}
