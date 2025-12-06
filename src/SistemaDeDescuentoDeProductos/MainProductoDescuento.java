ef38959e0cc45aab1ba5cbd11c192ad767acf1e3
SalinasManzanoYamilet <salinasmanzanoyamilet@gmail.com>
2025-12-05 20:53:09 -0600

    Add MainProductoDescuento class with main method

---
+++ src/SistemaDeDescuentoDeProductos/MainProductoDescuento.java
@@ -0,0 +1,6 @@
+public class MainProductoDescuento {
+    public static void main(String[] args) {
+        ControlProductoDescuento control = new ControlProductoDescuento();
+        control.ejecutar();
+    }
+}
